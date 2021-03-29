6436c9d171c3df0cbfe0da43c76a9db0246d01e5
Ali <alimanssouri221@gmail.com>
2021-03-29 21:08:23 +0430

    Landing page deploy

---
+++ components/search/Search.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { Dropdown, Tab, Tabs } from 'react-bootstrap'
+
+function Search() {
+    return (
+        <div className={`search-bar-wrap`}>
+            <Dropdown>
+                <Dropdown.Toggle as="div" id="dropdown-basic">
+                    <div className={`search-bar`}>
+                        <div className={`search-bar__box`}>
+                            <input
+                                type="text"
+                                className={`search-bar__box__input`}
+                                placeholder="جست و جو"
+                            />
+                            <button className={`search-bar__box__icon`}>
+                                <img
+                                    className={`search-bar__box__icon__img`}
+                                    src={`/image/icon/Magnifying glass.png`}
+                                    alt="search"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <div className={`search-bar__suggestion`}>
+                        <Tabs defaultActiveKey="موضوع" id="uncontrolled-tab-example">
+                            <Tab eventKey="موضوع" title="موضوع">
+                                <div className={`search-bar__suggestion__tab-content`}>
+                                    <div className={`search-bar__suggestion__tab-content-wrap`}>
+                                        <div className={`search-bar__suggestion__tab-content__pic`}>
+                                            <img
+                                                className={`search-bar__suggestion__tab-content__pic__img`}
+                                                src={`/image/book picture.png`}
+                                                alt="book"
+                                            />
+                                        </div>
+                                        <div className={`search-bar__suggestion__tab-content__box-wrap`}>
+                                            <div className={`search-bar__suggestion__tab-content__box`}>
+                                                <strong>نام کتاب: </strong>
+                                                <small>نام کتاب</small>
+                                            </div>
+                                            <div className={`search-bar__suggestion__tab-content__box`}>
+                                                <strong>نویسنده: </strong>
+                                                <small>نویسنده</small>
+                                            </div>
+                                        </div>
+                                        <div className={`search-bar__suggestion__tab-content__box-wrap`}>
+                                            <div className={`search-bar__suggestion__tab-content__box`}>
+                                                <strong>مترجم: </strong>
+                                                <small>مترجم</small>
+                                            </div>
+                                            <div className={`search-bar__suggestion__tab-content__box`}>
+                                                <strong>قیمت: </strong>
+                                                <small>قیمت</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Tab>
+                            <Tab eventKey="نویسنده" title="نویسنده">
+                                <div className={`search-bar__suggestion__tab-content`}>tab2</div>
+                            </Tab>
+                            <Tab eventKey="مترجم" title="مترجم">
+                                <div className={`search-bar__suggestion__tab-content`}>tab3</div>
+                            </Tab>
+                            <Tab eventKey="ناشر" title="ناشر">
+                                <div className={`search-bar__suggestion__tab-content`}>tab4</div>
+                            </Tab>
+                            <Tab eventKey="همه" title="همه">
+                                <div className={`search-bar__suggestion__tab-content`}>all</div>
+                            </Tab>
+                        </Tabs>
+                        <button className={`search-bar__suggestion__show-result`}>{`نمایش همه نتایج`}</button>
+                    </div>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+}
+
+export default Search
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,102 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import BasePage from '../components/BasePage'
 import GET_HOME_PAGE from '../gql/queries/get-home-page';
 import client from '../components/ApolloClient'
+import Search from '../components/search/Search'
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log(props.homePageData)
-  }
+const Home = (props) => {
 
-  static async getInitialProps({ query }) {
-    let homePageData = null;
+  const { homePageData } = props
+  const divRef = useRef(null);
 
-    try {
-      const result = await client.query({
-        query: GET_HOME_PAGE,
-        partialRefetch:true
-      });
-      homePageData = result.data.homepage
-    } catch (e) {
-      console.error(e)
-    }
+  const functionToBotHandler = () => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+  const functionToTopHandler = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-    return {
-      homePageData
-    }
-  }
+  useEffect(() => {
+    functionToBotHandler();
+    functionToTopHandler();
+  }, []);
 
-  render() {
-    const { homePageData } = this.props
-    return (
-      <BasePage
-        homePage
-        className={`home-page`}
-        seo={homePageData.seo}
-      >
-        <Link as={`/shop/slllluuuug`} href={`/shop/[[...slugs]]`}>
+  return (
+    <BasePage
+      homePage
+      className={`home-page`}
+      seo={homePageData.seo}
+    >
+      <div className={`landing`}>
+        {/* <LandingLoading /> */}
+        <div className={`landing-hero`}>
+          {/* <LandingHeader /> */}
+          <div className={`landing-hero__logo`}>
+            <img
+              className={`landing-hero__logo__img`}
+              src={`/image/LOGO TYPE 2.png`}
+              alt="landing"
+            />
+          </div>
+          <div className={`landing-hero__search`}>
+            <Search />
+          </div>
+          <div className={`landing-hero__to-bot`}>
+            <img
+              className={`landing-hero__to-bot__icon`}
+              onClick={functionToBotHandler}
+              src={`/image/icon/Group 2.png`}
+              alt="toBot"
+            />
+          </div>
+        </div>
+        <div style={{ marginTop: "-160px", height: "150px", overflow: "hidden" }}>
+          <svg
+            viewBox="0 0 500 150"
+            preserveAspectRatio="none"
+            style={{ height: "100%", width: "100%" }}
+          >
+            <path
+              d="M-4.79,174.17 C0.28,-21.20 468.68,168.25 501.97,-13.31 L500.00,150.00 L-51.06,183.05 Z"
+              style={{ stroke: "none", fill: "#1a91dc" }}
+            ></path>
+          </svg>
+        </div>
+        <div className={`landing-bottom-wrap`}>
+          <div className={`landing-bottom__left`}>
+            <div className={`landing-bottom__left__first`}>
+              <div className={`landing-bottom__left__box`}>{`مشاوره`}</div>
+              <div className={`landing-bottom__left__box`}>
+                {`کودک`} <br />{`و نوجـوان`}
+              </div>
+              <div className={`landing-bottom__left__box landing-bottom__left__box--disable`} >
+                {`هدیه`}
+              </div>
+            </div>
+            <div className={`landing-bottom__left__second`}>
+              <div className={`landing-bottom__left__box`}>خرید کتاب</div>
+              <div className={`landing-bottom__left__box`}>زبان</div>
+              <div className={`landing-bottom__left__box landing-bottom__left__box--disable`}>
+                {`بلاگ`}
+              </div>
+            </div>
+          </div>
+          <div className={`landing-bottom__right`}>
+            <div className={`landing-bottom__right__box`}></div>
+          </div>
+        </div>
+        <div ref={divRef} className={`return-to-top-wrap`}>
+          <button onClick={functionToTopHandler} className={`return-to-top`}>
+            <img src={`/image/icon/Polygon 1.png`} alt="arrow" />
+          </button>
+        </div>
+      </div>
+      {/* <Link as={`/shop/slllluuuug`} href={`/shop/[[...slugs]]`}>
           <a>
             {`Shop`}
           </a>
@@ -48,9 +106,28 @@ class Home extends React.Component {
           <a>
             {`Sample Page`}
           </a>
-        </Link>
-      </BasePage>
-    )
+        </Link> */}
+    </BasePage>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  let homePageData = null;
+
+  try {
+    const result = await client.query({
+      query: GET_HOME_PAGE,
+      partialRefetch: true
+    });
+    homePageData = result.data.homepage
+  } catch (e) {
+    console.error(e)
+  }
+
+  return {
+    props: {
+      homePageData
+    }
   }
 }
 
