61ce732edaf28e61405550348b306bf7109e1737
Ali <alimanssouri221@gmail.com>
2021-09-15 11:21:21 +0430

    Enamad Logo

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,12 +1,12 @@
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import {MDBCol, MDBContainer, MDBRow, MDBFooter} from "mdbreact";
 import Link from 'next/link'
 
 export default function Footer() {
     return (
-        <MDBFooter style={{direction:"rtl", backgroundColor: "antiquewhite"}} color="blue" className="font-small pt-4">
+        <MDBFooter style={{direction: "rtl", backgroundColor: "antiquewhite"}} color="blue" className="font-small pt-4">
             <MDBContainer fluid className="text-center text-md-right">
                 <MDBRow>
-                    <MDBCol md="6" >
+                    <MDBCol md="6">
                         <h5 className="title">
                             {`کتاب‌فروشی آنلاین داستانا`}
                         </h5>
@@ -14,7 +14,7 @@ export default function Footer() {
                             {`تیم داستانا با تجربۀ چندین سال کتابخوانی و کتابفروشی و فعالیت در حوزۀ نشر و تالیف و ترجمه، کار خود را از اواخر تابستان ۹۹ آغاز کرد. هدف تیم ما نخست ارائۀ محتواهای مفید و جذاب، مشاورۀ تخصصی در حوزۀ کتاب به کاربران برای انتخاب کتاب خوب و مناسب و سپس تسهیل فرآیند سفارش و خرید است.`}
                         </p>
                     </MDBCol>
-                    <MDBCol md="3">
+                    <MDBCol md="2">
                         <h5 className="title">{`خدمات مشتریان`}</h5>
                         <ul>
                             <li className="list-unstyled">
@@ -34,7 +34,7 @@ export default function Footer() {
                             </li>
                         </ul>
                     </MDBCol>
-                    <MDBCol md="3">
+                    <MDBCol md="2">
                         <h5 className="title">{`اطلاعات داستانا`}</h5>
                         <ul>
                             <li className="list-unstyled">
@@ -54,13 +54,21 @@ export default function Footer() {
                             </li>
                         </ul>
                     </MDBCol>
+                    <MDBCol md="2" style={{textAlign: "center"}}>
+                        <a referrerPolicy="origin" target="_blank"
+                           href="https://trustseal.enamad.ir/?id=230991&amp;Code=E8Sfoi6CstYu00ZHWKKW"><img
+                            referrerPolicy="origin"
+                            src="https://Trustseal.eNamad.ir/logo.aspx?id=230991&amp;Code=E8Sfoi6CstYu00ZHWKKW" alt=""
+                            style={{cursor:"pointer"}} id="E8Sfoi6CstYu00ZHWKKW"/></a>
+                    </MDBCol>
                 </MDBRow>
             </MDBContainer>
             <div style={{backgroundColor: "#c5c5c5"}} className="footer-copyright text-center py-3">
                 <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: <a style={{color: "#0072bb"}} href="https://dastanaa.com"> Dastanaa.com </a>
+                    &copy; {new Date().getFullYear()} Copyright: <a style={{color: "#0072bb"}}
+                                                                    href="https://dastanaa.com"> Dastanaa.com </a>
                 </MDBContainer>
             </div>
         </MDBFooter>
-    )
+)
 }
