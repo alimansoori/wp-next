29b4916531c8406d7ebe20e59e1be8da6354f8ee
Ali <alimanssouri221@gmail.com>
2021-10-24 23:44:59 +0330

    error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,16 @@
-
-
-function Error({ statusCode }) {
+function Error({statusCode}) {
     return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
+        <p>
+            {statusCode
+                ? `An error ${statusCode} occurred on server`
+                : 'متاسفانه با خطایی مواجه شدیم. لطفا صفحه را تازه سازی کنید.'}
+        </p>
     )
-  }
-  
-  Error.getInitialProps = ({ res, err }) => {
+}
+
+Error.getInitialProps = ({res, err}) => {
     const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return { statusCode }
-  }
-  
-  export default Error
+    return {statusCode}
+}
+
+export default Error
